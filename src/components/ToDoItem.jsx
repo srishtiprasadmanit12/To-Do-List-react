007175da9c7fc3a5031d6e7975d81b632c08a195
srishti <srishtiprasad500@gmail.com>
2022-05-25 13:10:41 +0530

    cross completed item on clicking

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,8 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 
 function ToDoItem(props){
+
+const [isDone,setDone]=useState(false);
+function handleClick(){
+    setDone((prevVal)=>{
+        return !prevVal;
+    })
+}
+
     return(
-        <li>{props.text}</li>
+        <div onClick={handleClick}>
+            <li style={{textDecoration:isDone?"line-through":"none"}}>{props.text}</li>
+        </div>
+        
     );
 }
 export default ToDoItem;
